b2db3c93ec846d86eb97a0bcccc89f16c1b1a4eb
alexpg2019 <05jandro@gmail.com>
2024-05-14 16:52:11 +0200

    Hola

--- JavaBnB/src/main/java/javabnb/backend/JavaBnB.java
+++ JavaBnB/src/main/java/javabnb/backend/JavaBnB.java
@@ -16,5 +16,5 @@ public class JavaBnB {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    public void setCaca(){System.out.println("Hello World!");}
+    public void setCaca(){System.out.println("Hello d!");}
 }
